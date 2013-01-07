925e8e05f2b14121f5d4e22b067e0ba7040cab81
pirjohh <pirjo.haakana@kolumbus.fi>
2013-01-07 11:45:16 +0200

    olen testannut kommentin lisäystä

--- vasso/vasso/urls.py
+++ vasso/vasso/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+# Pirjo: laitanpa tähän kokeeksi kommenttilisäyksen 
 
 urlpatterns = patterns('',
     # Examples:
